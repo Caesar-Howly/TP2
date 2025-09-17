62ed8cc6bf6de1d86d92c4387cb752d8f2847571
howlyc <howlyc@educ.college-montreal.qc.ca>
2025-09-17 16:19:06 -0400

    Jeu de chance. Quelques erreurs a corriger.

--- TP2.py
+++ TP2.py
@@ -1,1 +1,30 @@
-print("")
+import random
+
+
+game_begin = True
+essaies = 1
+result = random.randint(0, 10)
+
+while game_begin:
+
+    x = int(input("Choisissez un nombre de 0 a 10:"))
+
+    if x > result:
+        print("Le nombre est trop grande. essayez une autre fois:")
+        essaies += 1
+    elif x < result:
+        print("Le nombre est trop petite. Essayez une autre fois:")
+        essaies += 1
+    else:
+        print(f"Bravo! Vous avez eu la bonne réponse en {essaies} essaies.")
+        quitter = input("Voulez-vous continuer? (o/n)")
+
+        if quitter == "o":
+            print("ok")
+
+        else:
+            game_begin = False
+
+
+print("ok bye")
+
