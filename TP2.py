d288ea8e28b22585dc0d1616a5c9e6caacdfa8fe
howlyc <howlyc@educ.college-montreal.qc.ca>
2025-09-19 12:09:34 -0400

    Jeu de chance. Corriger erreur pour avoir ecrit str au lieu de int

--- TP2.py
+++ TP2.py
@@ -1,30 +1,67 @@
+"""
+Nom = Caesar
+Gr = 407
+
+Ce code est un jeu de chance ou le joueur devrait deviner le nombre que l'ordinateur a choisi par hasard de 0-1000.
+"""
+
+
 import random
 
 
 game_begin = True
-essaies = 1
-result = random.randint(0, 10)
+essaies = 0
+#result = random.randint(0, 100)
+
+#input("Choissisez la difficulté du jeu: Facile, Moyenne, Difficile ( Ecrivez le premier lettre en MAJUSCULE")
+
+
+#if input() == "Facile":
+   # random.randint(0, 100)
+#elif input() == "Moyenne":
+    #random.randint(0, 1000)
+#elif input() == "Difficile":
+   # random.randint(0, 10000)
+
+def gener(x,y):
+    answer = random.randint(x, y)
+    return answer
+
+
+first = int(input("first number"))
+last = int(input("last number"))
+
+
+result = gener(first, last)
+
 
 while game_begin:
 
-    x = int(input("Choisissez un nombre de 0 a 10:"))
+    essaies += 1
+
+    x = int(input("Choisissez un nombre de 0 a 1000:"))
 
     if x > result:
-        print("Le nombre est trop grande. essayez une autre fois:")
-        essaies += 1
+        print(f"{x} est trop grande. essayez une autre fois:")
+
     elif x < result:
-        print("Le nombre est trop petite. Essayez une autre fois:")
-        essaies += 1
+        print(f"{x} est trop petite. Essayez une autre fois:")
+
     else:
         print(f"Bravo! Vous avez eu la bonne réponse en {essaies} essaies.")
         quitter = input("Voulez-vous continuer? (o/n)")
 
         if quitter == "o":
             print("ok")
+            #result = random.randint(0, 100)
+            first = int(input("first number"))
+            last = int(input("last number"))
+
+            result = random.randint(first, last)
+            essaies = 0
 
         else:
             game_begin = False
 
 
 print("ok bye")
-
