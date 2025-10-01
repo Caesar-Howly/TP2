5adae5cb572481a904cd6c1eda4b27ca789a05e1
howlyc <howlyc@educ.college-montreal.qc.ca>
2025-10-01 10:37:18 -0400

    Jeu de chance. Ajoute de choix de difficulté.

--- TP2.py
+++ TP2.py
@@ -20,9 +20,14 @@ last = 100
 def generate():
     global first
     global last
-    first = int(input("Choisissez votre premier nombre:"))
-    last = int(input("Choisissez votre dernier nombre: "))
-    print(f"J'ai choisi un numéro entre les nombres de votre choix. Choisissez un nombre de {first} à {last}:")
+    difficulty = str(input("Voulez-vous garder la difficulté défaute? o/n"))
+
+    if difficulty == "o":
+        print(f"J'ai choisi un numéro entre les nombres de votre choix. Choisissez un nombre de {first} à {last}:")
+    else:
+        first = int(input("Choisissez votre premier nombre:"))
+        last = int(input("Choisissez votre dernier nombre: "))
+        print(f"J'ai choisi un numéro entre les nombres de votre choix. Choisissez un nombre de {first} à {last}:")
 
 
 generate()
